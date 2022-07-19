bf4d6bc9bd91a1094e33032e8f38e6494851f513
Yun Long <longdeqidao@gmail.com>
2022-07-19 15:28:46 +0800

    Update config

--- demo-canary.sh
+++ demo-canary.sh
@@ -1,56 +1,127 @@
 #!/usr/bin/env bash
 
-# 1. Team-delivery
+# 1. Deploy primary stack
+kubectl apply -f deploy/mesh/k8s_order.yaml
+kubectl apply -f meshdemo-restaurant/k8s_restaurant.yaml
+kubectl apply -f deploy/mesh/k8s_delivery.yaml
 
-# 1.1 Check primary traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}'
+# Primary traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}'
 
-# 1.2 Deploy delivery Beijing canary.
+# 2. Deploy canary Delivery Beijing (Green)
 kubectl apply -f deploy/mesh/k8s_delivery_beijing.yaml
 
-# 1.3 Check Beijing traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing'
+echo '
+apiVersion: mesh.megaease.com/v1alpha1
+kind: ServiceCanary
+metadata:
+  name: beijing
+spec:
+  priority: 3
+  selector:
+    matchServices: [delivery-mesh]
+    matchInstanceLabels: {release: delivery-mesh-beijing}
+  trafficRules:
+    headers:
+      X-Location:
+        exact: Beijing
+' | emctl apply -f -
 
-# 1.4 Apply service canary rules(This should be done before 1.3).
-emctl apply -f deploy/mesh/easemesh_delivery_beijing.yaml
+# Beijing traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing'
 
-# 1.5 Check Beijing traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing'
-
-#----------------------------------------------------------------------------------
-
-# 2. Team-restaurant
-
-# 2.1 Deploy restaurant Beijing canary.
-kubectl apply -f deploy/mesh/k8s_restaurant_beijing.yaml
-
-# 2.2 Check Beijing traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing'
-
-# 2.3 Apply service canary rules(this should be done before 2.1).
-emctl apply -f deploy/mesh/easemesh_restaurant_beijing.yaml
-
-# 2.4 Check Beijing traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing'
-
-# 2.5 Check Beijing traffic with specifying as restaurant canary.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing' -H 'X-Mesh-Service-Canary: restaurant-mesh-beijing'
-
-#----------------------------------------------------------------------------------
-
-# Team-delivery co-operates with Team-restaurant
-
-# 3.1 Check Android traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Phone-Os: Android'
-
-# 3.2 Apply service canary rules.
-emctl apply -f deploy/mesh/easemesh_android.yaml
-
-# 3.3 Deploy delivery Android canary.
+# 3. Deploy canary Restaurant/Delivery Android
 kubectl apply -f deploy/mesh/k8s_delivery_android.yaml
+kubectl apply -f meshdemo-restaurant/k8s_restaurant_android.yaml
 
-# 3.4 Deploy restaurant Android canary.
-kubectl apply -f deploy/mesh/k8s_restaurant_android.yaml
+echo '
+apiVersion: mesh.megaease.com/v1alpha1
+kind: ServiceCanary
+metadata:
+  name: android
+spec:
+  priority: 2
+  selector:
+    matchServices: [restaurant-mesh, delivery-mesh]
+    matchInstanceLabels: {release: refund-android}
+  trafficRules:
+    headers:
+      X-Phone-Os:
+        exact: Android
+' | emctl apply -f -
 
-# 3.5 Check Android traffic.
-curl http://127.0.0.1:32539/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Phone-Os: Android'
+# Android traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Phone-Os: Android'
+
+# 4. Deploy canary Restaurant Beijing&Android
+kubectl apply -f meshdemo-restaurant/k8s_restaurant_beijing_android.yaml
+
+echo '
+apiVersion: mesh.megaease.com/v1alpha1
+kind: ServiceCanary
+metadata:
+  name: beijing-android
+spec:
+  priority: 1
+  selector:
+    matchServices: [restaurant-mesh]
+    matchInstanceLabels: {release: restaurant-mesh-beijing-android}
+  trafficRules:
+    headers:
+      X-Location:
+        exact: Beijing
+      X-Phone-Os:
+        exact: Android
+' | emctl apply -f -
+
+# Beijing&Android traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing' -H 'X-Phone-Os: Android'
+
+# 5. Change priorty
+
+echo '
+apiVersion: mesh.megaease.com/v1alpha1
+kind: ServiceCanary
+metadata:
+  name: beijing-android
+spec:
+  priority: 2
+  selector:
+    matchServices: [restaurant-mesh]
+    matchInstanceLabels: {release: restaurant-mesh-beijing-android}
+  trafficRules:
+    headers:
+      X-Location:
+        exact: Beijing
+      X-Phone-Os:
+        exact: Android
+' | emctl apply -f -
+
+echo '
+apiVersion: mesh.megaease.com/v1alpha1
+kind: ServiceCanary
+metadata:
+  name: android
+spec:
+  priority: 1
+  selector:
+    matchServices: [restaurant-mesh, delivery-mesh]
+    matchInstanceLabels: {release: refund-android}
+  trafficRules:
+    headers:
+      X-Phone-Os:
+        exact: Android
+' | emctl apply -f -
+
+# Check all kinds of traffic.
+# Primary Traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}'
+
+# Beijing Traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing'
+
+# Android Traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Phone-Os: Android'
+
+# Beijing&Android traffic
+curl http://127.0.0.1:30188/ -d '{"order_id": "abc1234", "food": "bread"}' -H 'X-Location: Beijing' -H 'X-Phone-Os: Android'
